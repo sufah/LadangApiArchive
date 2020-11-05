a2d7579c8affb736dd2a1f2b11b33abb87c3bc56
SU FAH LIAU <liau@sufah.net>
2020-11-05 12:09:22 +0800

    Plantation Model set up

---
+++ Models/Plantation.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.PortableExecutable;
+
+namespace LadangApi.Models
+{
+    public class Plantation
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public double Rate { get; set; }
+        
+        [Required]
+        public string Location { get; set; }
+        
+        [Required]
+        public string Vacancy { get; set; }
+        
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Phone Format is not valid")]
+        public string Phone { get; set; }
+        
+        [Required]
+        public int PalmAge { get; set; }
+        
+        public string ImagePath { get; set; }
+        
+        [NotMapped] 
+        public byte[] ImageArray { get; set; }
+        
+        public string UserId { get; set; }
+    }
+}
