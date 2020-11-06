247c253539d55a5a0377ff8bd370e9e0529c5f50
SU FAH LIAU <liau@sufah.net>
2020-11-06 13:08:35 +0800

    created dbContext to access database.

---
+++ Controllers/PlantationsController.cs
@@ -0,0 +1,54 @@
+﻿using LadangApi.Data;
+using LadangApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LadangApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantationsController : ControllerBase
+    {
+        private PlantationDbContext _plantationDbContext;
+
+        public PlantationsController(PlantationDbContext plantationDbContext)
+        {
+            _plantationDbContext = plantationDbContext;
+        }
+
+
+        // GET: api/<PlantationsController>
+        [HttpGet]
+        public IEnumerable<Plantation> Get()
+        {
+            return _plantationDbContext.Plantations;
+        }
+
+        // GET api/<PlantationsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PlantationsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PlantationsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PlantationsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
