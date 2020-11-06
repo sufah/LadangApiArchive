2d45b89fb2fedb9b05aad44db69715193779655d
SU FAH LIAU <liau@sufah.net>
2020-11-06 17:59:13 +0800

    Set up the unit test files and nuget packages

---
+++ LadangApi.UnitTests/Controllers/PlantationsControllerTests.cs
@@ -0,0 +1,19 @@
+﻿
+using NUnit.Framework;
+
+namespace LadangApi.UnitTests.Controllers
+{
+    [TestFixture]
+    public class PlantationsControllerTests
+    {
+        [Test]
+        public void Get_PlantationList_ReturnTrue()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
