6cbae6891675a3a0486d740ebb8060174ba3ad32
SU FAH LIAU <liau@sufah.net>
2020-11-07 16:01:56 +0800

    Created IPlantationDbContext.cs

--- Data/PlantationDbContext.cs
+++ Data/PlantationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using LadangApi.Models;
+﻿using LadangApi.Interface;
+using LadangApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace LadangApi.Data
 {
-    public class PlantationDbContext : DbContext
+    public class PlantationDbContext : DbContext, IPlantationDbContext
     {
         public PlantationDbContext(DbContextOptions<PlantationDbContext> options) : base(options)
         {
---
+++ Interface/IPlantationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using LadangApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LadangApi.Interface
+{
+    public interface IPlantationDbContext
+    {
+        DbSet<Plantation> Plantations { get; set; }
+    }
+}
